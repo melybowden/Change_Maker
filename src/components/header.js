f7a3928ccba5449e8acd07102845fc117c0873ed
Nate <nathan.smiley4@gmail.com>
2021-07-21 16:38:39 -0400

    add header

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import logo from './logo.jpg'
+
 
 export default function header() {
     return (
         <div>
-            
-        </div>
+            <img src={logo}/>
+            <h1> Change Maker Calculator  Times Clicked:  </h1> 
+      
+            </div>
     )
 }
